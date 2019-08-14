80497be7271fe6fef3909f6b8e876bc493529544
Non Dewman <Dnewman1934@yahoo.com>
2019-08-14 16:47:32 -0500

    Auto dploy check

---
+++ client/build/precache-manifest.f2decfee12838656a24fcc5b1ac58592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c923ce44e15025d763e",
+    "url": "/static/js/main.230ba6d0.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5c923ce44e15025d763e",
+    "url": "/static/css/main.77803e3a.chunk.css"
+  },
+  {
+    "revision": "bb5ea0c87ddc921223087332afaffb79",
+    "url": "/index.html"
+  }
+];
