63ba81fbe9368b42a4c98c42899caf0b7c8f538a
Non Dewman <Dnewman1934@yahoo.com>
2019-08-15 11:19:44 -0500

    item cards, flex

--- client/src/components/shop.js
+++ client/src/components/shop.js
@@ -11,31 +11,134 @@ class Shop extends Component {
     toggleCategories() {
         if (this.state.activeTab === 0) {
             return (
-                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                    <CardTitle style={{
-                        color: '#fff', height: '176',
-                        background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
-                    }} > Necklace Name</CardTitle>
-                    <CardText>
-                        This fine necklace is dope please click this to creep
+                <div className="productcard-grid">
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Necklace Name</CardTitle>
+                        <CardText>
+                            This fine necklace is dope please click this to creep
                     </CardText>
-                    <CardActions>
-                        <Button>Add to Cart</Button>
-                    </CardActions>
-                </Card>
+                        <CardActions>
+                            <Button>View Item</Button>
+
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Necklace Name</CardTitle>
+                        <CardText>
+                            This fine necklace is also dope please click this to creep
+                    </CardText>
+                        <CardActions>
+                            <Button>View Item</Button>
+
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Necklace Name</CardTitle>
+                        <CardText>
+                            This fine necklace is SUPER dope please click this to creep
+                    </CardText>
+                        <CardActions>
+                            <Button>View Item</Button>
+
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                </div>
             )
         }
         else if (this.state.activeTab === 1) {
             return (
-                <div>
-                    <h1>These are bracelets</h1>
+                <div className="productcard-grid">
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Bracelet Name</CardTitle>
+                        <CardText>
+                            This fine Bracelet is dope please click this to creep
+                </CardText>
+                        <CardActions>
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Bracelet Name</CardTitle>
+                        <CardText>
+                            This fine bracelet is also dope please click this to creep
+                </CardText>
+                        <CardActions>
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > bracelet Name</CardTitle>
+                        <CardText>
+                            This fine bracelet is SUPER dope please click this to creep
+                </CardText>
+                        <CardActions>
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
                 </div>
             )
         }
         else if (this.state.activeTab === 2) {
             return (
-                <div>
-                    <h1>These are earrings</h1>
+                <div className="productcard-grid">
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Earring Name</CardTitle>
+                        <CardText>
+                            This fine Earring is dope please click this to creep
+                </CardText>
+                        <CardActions>
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Earring Name</CardTitle>
+                        <CardText>
+                            This fine Earring is also dope please click this to creep
+                </CardText>
+                        <CardActions>
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{
+                            color: '#fff', height: '176',
+                            background: "url(https://i.etsystatic.com/isbl/d488ef/35812320/isbl_1680x420.35812320_qzupim8o.jpg?version=0) center/cover "
+                        }} > Earring Name</CardTitle>
+                        <CardText>
+                            This fine Earring is SUPER dope please click this to creep
+                </CardText>
+                        <CardActions>
+                            <Button>View Item</Button>
+                            <Button>Add to Cart</Button>
+                        </CardActions>
+                    </Card>
                 </div>
             )
         }
@@ -49,7 +152,7 @@ class Shop extends Component {
                     <Tab>Earrings</Tab>
                 </Tabs>
                 <section>
-                    <Grid className="productcard-grid">
+                    <Grid >
                         <Cell col={12}>
                             <div className="content">{this.toggleCategories()}
                             </div>
